28c6aa918e26eeb1e0a4c793f9b28466d70821b5
mezoBear <52471898+mezoBear@users.noreply.github.com>
2021-05-24 03:10:25 +0530

    Completed-roll-a-zombie
    Completed  roll a zombie game

---
+++ 1.Roll-A-Zombie/Assets/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameEngine gameEngine;
+    public AudioClip hit;
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        gameEngine.AddPoint();
+        source.PlayOneShot(hit);
+    }
+}
